797abb496f025180ce2481057f17d426065d9eb4
victorsbd <victorsbd@gmail.com>
2019-05-02 19:12:13 +0200

    Added project to use the wrapper with EmguCv instead of OpenCvSharp.

---
+++ YoloEmguCv/Wrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace YoloEmguCv
+{
+    internal static class Wrapper
+    {
+        private const string DllName = "YoloApi";
+
+        // same as c++ struct bbox_t from yolo_v2_class
+        // need to disable check for Field XYZ is never assigned to, and will always have its default value XX
+        // because the struct is initialized in the c++ code
+        #pragma warning disable 0649
+        internal struct BoundingBox
+        {
+            internal uint X, Y, W, H;
+            internal float Prob;
+            internal uint ObjId;
+            internal uint TrackId;
+            internal uint FramesCounter;
+        };
+        #pragma warning restore 0649
+
+        [DllImport(DllName, EntryPoint = "initDetector", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void Init(string cfgFile, string weightFile, int gpuId = 0);
+        [DllImport(DllName, EntryPoint = "closeDetector", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void Close();
+        [DllImport(DllName, EntryPoint = "detect", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern unsafe void Detect(IntPtr matPtr, int matRows, int matCols, float thresh, bool useMean, out BoundingBox* elems, out int elemsSize);
+    }
+}
---
+++ YoloEmguCv/YoloResult.cs
@@ -0,0 +1,60 @@
+﻿namespace YoloEmguCv
+{
+    /// <summary>
+    /// Represents the detection information of an object by Yolo
+    /// </summary>
+    public class YoloResult
+    {
+        /// <summary>
+        /// X coordinate of the top-left corner of bounded box
+        /// </summary>
+        public uint X { get; }
+
+        /// <summary>
+        /// Y coordinate of the top-left corner of bounded box
+        /// </summary>
+        public uint Y { get; }
+
+        /// <summary>
+        /// Width of bounded box
+        /// </summary>
+        public uint Width { get; }
+
+        /// <summary>
+        /// Height of bounded box
+        /// </summary>
+        public uint Height { get; }
+
+        /// <summary>
+        /// Probability that the object was found correctly
+        /// </summary>
+        public float Prob { get; }
+
+        /// <summary>
+        /// Class of object - from range [0, classes-1]
+        /// </summary>
+        public uint ObjId { get; }
+
+        /// <summary>
+        /// Tracking id for video (0 - untracked, 1 - inf - tracked object)
+        /// </summary>
+        public uint TrackId { get; }
+
+        /// <summary>
+        /// Counter of frames on which the object was detected
+        /// </summary>
+        public uint FramesCounter { get; }
+
+        internal YoloResult(uint x, uint y, uint width, uint height, float prob, uint objId, uint trackId, uint framesCounter)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Prob = prob;
+            ObjId = objId;
+            TrackId = trackId;
+            FramesCounter = framesCounter;
+        }
+    }
+}
